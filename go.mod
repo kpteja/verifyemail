4bd3a9a2ccecab77a180666a35a302637301da50
Poorna Teja Konatham <kpteja@outlook.com>
2020-08-25 16:22:45 +0530

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kpteja/verifyemail
+
+go 1.14
+
+require github.com/badoux/checkmail v1.2.0
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/badoux/checkmail"
+)
+
+func main() {
+	email := flag.String("email", "", "Email to verify")
+	flag.Parse()
+
+	// Validate format.
+	if err := checkmail.ValidateFormat(*email); err != nil {
+		fmt.Println("ERROR:", err)
+		return
+	}
+	fmt.Println("Format OK")
+
+	// Validate domain.
+	if err := checkmail.ValidateHost(*email); err != nil {
+		fmt.Println("ERROR:", err)
+		return
+	}
+	fmt.Println("Domain OK")
+
+	// Validate user.
+	err := checkmail.ValidateHost(*email)
+	if smtpErr, ok := err.(checkmail.SmtpError); ok && err != nil {
+		fmt.Printf("ERROR: Code: %s, Msg: %s\n", smtpErr.Code(), smtpErr)
+	}
+	fmt.Println("User OK")
+}
