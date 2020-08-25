1d3f820f94ca290c2189c2d75bdb82f9aa84eea7
Poorna Teja Konatham <kpteja@outlook.com>
2020-08-25 17:43:23 +0530

    debug: stop returning

--- main.go
+++ main.go
@@ -14,21 +14,24 @@ func main() {
 	// Validate format.
 	if err := checkmail.ValidateFormat(*email); err != nil {
 		fmt.Println("ERROR:", err)
-		return
+		// return
+	} else {
+		fmt.Println("Format OK")
 	}
-	fmt.Println("Format OK")
 
 	// Validate domain.
 	if err := checkmail.ValidateHost(*email); err != nil {
 		fmt.Println("ERROR:", err)
-		return
+		// return
+	} else {
+		fmt.Println("Domain OK")
 	}
-	fmt.Println("Domain OK")
 
 	// Validate user.
 	err := checkmail.ValidateHost(*email)
 	if smtpErr, ok := err.(checkmail.SmtpError); ok && err != nil {
 		fmt.Printf("ERROR: Code: %s, Msg: %s\n", smtpErr.Code(), smtpErr)
+	} else {
+		fmt.Println("User OK")
 	}
-	fmt.Println("User OK")
 }
